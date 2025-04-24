2177ab17716522f98d4eef37a3f8ca0289706459
MesservonSchwanz <szabolcsszatmari12@gmail.com>
2025-04-24 13:06:08 +0200

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: "./src",
+  base: '/MoodifyCleanFinal/',
   build: {
     outDir: "../dist",
+    emptyOutDir: true
   }
 })
