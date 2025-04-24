48937a612da7dc6f563cdb8b63dc4a902caaf750
MesservonSchwanz <szabolcsszatmari12@gmail.com>
2025-04-24 13:35:00 +0200

    Fix: proper build output to root for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: "/MoodifyCleanFinal/",
-  root: "./src",
   build: {
-    outDir: "../dist",
+    outDir: "dist"
   }
 })
